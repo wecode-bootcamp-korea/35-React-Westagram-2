380d994ce1a4ff8d44384cffc66c9a5b7417b211
och.changhoon <och.changhoon@gmail.com>
2022-07-06 15:29:45 +0900

    fixed: 2차 수정 완료

--- src/Router.js
+++ src/Router.js
@@ -1,34 +1,33 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LoginChang from './pages/changhoonOh/Login/Login'
-import MainChang from './pages/changhoonOh/Main/Main'
-import LoginHoon from './pages/hoonjoJeong/Login/Login'
-import MainHoon from './pages/hoonjoJeong/Main/Main'
-import LoginKwang from './pages/kwangheeKim/Login/Login'
-import MainKwang from './pages/kwangheeKim/Main/Main'
-import LoginSeong from './pages/seongeunPark/Login/Login'
-import MainSeong from './pages/seongeunPark/Main/Main'
-import LoginWon from './pages/wonikChoi/Login/Login'
-import MainWon from './pages/wonikChoi/Main/Main'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginChang from './pages/changhoonOh/Login/Login';
+import MainChang from './pages/changhoonOh/Main/Main';
+import LoginHoon from './pages/hoonjoJeong/Login/Login';
+import MainHoon from './pages/hoonjoJeong/Main/Main';
+import LoginKwang from './pages/kwangheeKim/Login/Login';
+import MainKwang from './pages/kwangheeKim/Main/Main';
+import LoginSeong from './pages/seongeunPark/Login/Login';
+import MainSeong from './pages/seongeunPark/Main/Main';
+import LoginWon from './pages/wonikChoi/Login/Login';
+import MainWon from './pages/wonikChoi/Main/Main';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login-chang' element={<LoginChang />} />
-        <Route path='/main-chang' element={<MainChang />} />
-        <Route path='/login-hoon' element={<LoginHoon />} />
-        <Route path='/main-hoon' element={<MainHoon />} />
-        <Route path='/login-kwang' element={<LoginKwang />} />
-        <Route path='/main-kwang' element={<MainKwang />} />
-        <Route path='/login-seong' element={<LoginSeong />} />
-        <Route path='/main-seong' element={<MainSeong />} />
-        <Route path='/login-won' element={<LoginWon />} />
-        <Route path='/main-won' element={<MainWon />} />
+        <Route path="/login-chang" element={<LoginChang />} />
+        <Route path="/main-chang" element={<MainChang />} />
+        <Route path="/login-hoon" element={<LoginHoon />} />
+        <Route path="/main-hoon" element={<MainHoon />} />
+        <Route path="/login-kwang" element={<LoginKwang />} />
+        <Route path="/main-kwang" element={<MainKwang />} />
+        <Route path="/login-seong" element={<LoginSeong />} />
+        <Route path="/main-seong" element={<MainSeong />} />
+        <Route path="/login-won" element={<LoginWon />} />
+        <Route path="/main-won" element={<MainWon />} />
       </Routes>
-
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default Router;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 function Nav() {
-    return <div>나 응애나브</div>
+  return <div>나 응애나브</div>;
 }
 
 export default Nav;

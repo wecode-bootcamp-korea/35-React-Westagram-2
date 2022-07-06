0ea6f412cf76abe026af73ccd13ab6a73f6408d9
Yelihi <Yelihi19@gmail.com>
2022-07-06 17:29:52 +0900

    Add: login, main 추가

--- src/pages/wonikChoi/Login/Login.js
+++ src/pages/wonikChoi/Login/Login.js
@@ -1,7 +1,141 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
+import '../../../styles/reset.scss';
 
-function LoginWon() {
-  return <h1>헬?루</h1>;
-}
+// 로그인 부분 사진 변동시키기
+
+const LoginWon = () => {
+  const [number, setNumber] = useState(1);
+  const [valueId, setValueId] = useState('');
+  const [valuePw, setValuePw] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [focus, setFocus] = useState('');
+  const interval = useRef();
+  const navigate = useNavigate();
+
+  const cycleImage = () => {
+    if (number === 1) {
+      setNumber(2);
+    } else if (number === 2) {
+      setNumber(3);
+    } else if (number === 3) {
+      setNumber(1);
+    }
+  };
+
+  useEffect(() => {
+    interval.current = setInterval(cycleImage, 3000);
+    console.log('시작');
+    return () => {
+      console.log('끝');
+      clearInterval(interval.current);
+    };
+  }, [number]);
+
+  const onChangeId = e => {
+    setValueId(e.target.value);
+  };
+
+  const onChangePw = e => {
+    setValuePw(e.target.value);
+  };
+
+  const onkey = e => {
+    const valueArr = valueId.split('');
+    if (valueArr.some(elm => elm === '@') && valuePw.length > 5) {
+      setDisabled(false);
+      setFocus('focus');
+    } else {
+      setDisabled(true);
+      setFocus('');
+    }
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    navigate('/main-won');
+  };
+
+  return (
+    <>
+      <div className="login">
+        <main className="container">
+          <article className="container-center">
+            <div className="background_image">
+              <div className="image-container">
+                <img
+                  className="image active"
+                  src={`./images/wonikChoi/cover-instagram${number}.png`}
+                />
+                <img
+                  className="image active"
+                  src={`./images/wonikChoi/cover-instagram${number}.png`}
+                />
+                <img
+                  className="image active"
+                  src={`./images/wonikChoi/cover-instagram${number}.png`}
+                />
+              </div>
+            </div>
+            <div className="login-container">
+              <div className="login-container__input">
+                <div className="login-logo">
+                  <span>Westagram</span>
+                </div>
+                <form
+                  onKeyUp={onkey}
+                  onSubmit={onSubmit}
+                  action=""
+                  className="login-input"
+                >
+                  <input
+                    type="text"
+                    className="login-input__ID"
+                    placeholder="전화번호, 사용자 이름 또는 이메일"
+                    value={valueId}
+                    onChange={onChangeId}
+                  />
+                  <input
+                    type="password"
+                    className="login-input__PW"
+                    placeholder="비밀번호"
+                    value={valuePw}
+                    onChange={onChangePw}
+                  />
+                  <button
+                    type="submit"
+                    className={`login-button ${focus}`}
+                    disabled={disabled}
+                  >
+                    로그인
+                  </button>
+                </form>
+                <div className="login-separate">
+                  <div className="login-separate__line"></div>
+                  <div className="login-separate__span">또는</div>
+                  <div className="login-separate__line"></div>
+                </div>
+                <div className="login-facebook">
+                  <div className="login-facebook__img"></div>
+                  <span>Facebook으로 로그인</span>
+                </div>
+                <div className="login-lostPassword">
+                  <span>비밀번호를 잊으셨나요?</span>
+                </div>
+              </div>
+              <div className="login-container__inroll">
+                <div className="login-inroll__span">
+                  <p>계정이 없으신가요?</p>
+                  <span>가입하기</span>
+                </div>
+              </div>
+            </div>
+          </article>
+        </main>
+      </div>
+    </>
+  );
+};
 
 export default LoginWon;
--- src/pages/wonikChoi/Main/Main.js
+++ src/pages/wonikChoi/Main/Main.js
@@ -1,7 +1,489 @@
-import './Main.scss';
+import React, { useState, useReducer } from 'react';
+import Comment from './Comment';
 
-function MainWon() {
-  return <h1>헬?루</h1>;
-}
+import './Main.scss';
+import '../../../styles/reset.scss';
+
+// 검색창 포커스 됬을 경우
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'create-message':
+      const comment = action.payload.comment;
+      const newComment = {
+        id: Date.now(),
+        comment: comment,
+        like: false,
+      };
+
+      return {
+        user: [...state.user, newComment],
+      };
+    case 'delete-message':
+      return {
+        user: state.user.filter(comment => comment.id !== action.payload.id),
+      };
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  user: [],
+};
+
+const MainWon = () => {
+  const [comment, setComment] = useState('');
+  const [commentInfo, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <div className="main">
+        <nav className="nav">
+          <div className="nav-container">
+            <div className="nav-container__logo">
+              <div className="nav-logo">Westagram</div>
+            </div>
+            <div className="nav-container__search">
+              <div className="search-input">
+                <input
+                  type="text"
+                  id="input"
+                  autocomplete="off"
+                  className="search-input__input"
+                  placeholder="검색"
+                />
+                <i className="bx bx-search" id="search"></i>
+                <i className="bx bxs-x-circle" id="exit"></i>
+              </div>
+            </div>
+            <div className="nav-container__icon">
+              <div className="nav-icon">
+                <a href="#">
+                  <i className="bx bxs-home"></i>
+                </a>
+              </div>
+              <div className="nav-icon">
+                <a>
+                  <i className="bx bx-paper-plane"></i>
+                </a>
+              </div>
+              <div className="nav-icon">
+                <a>
+                  <i className="bx bx-plus-circle"></i>
+                </a>
+              </div>
+              <div className="nav-icon">
+                <a>
+                  <i className="bx bx-compass"></i>
+                </a>
+              </div>
+              <div className="nav-icon">
+                <a>
+                  <i className="bx bx-heart"></i>
+                </a>
+              </div>
+              <div className="nav-profile__image">
+                <a>
+                  <img src="./images/wonikChoi/profile/myprofile.jpeg" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <section className="section-container">
+          <div className="main-container">
+            <div className="top-article">
+              <div className="top-article__container">
+                <div className="top-article__info">
+                  <div className="info-image">
+                    <img
+                      src="./images/wonikChoi/profile/elvis-ray-J-JOGvnfgM8-unsplash.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="info-content">Elvis_lay</div>
+                </div>
+                <div className="top-article__info">
+                  <div className="info-image">
+                    <img
+                      src="./images/wonikChoi/profile/anh-nguyen-kcA-c3f_3FE-unsplash.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="info-content">cover-vesi</div>
+                </div>
+                <div className="top-article__info">
+                  <div className="info-image">
+                    <img
+                      src="./images/wonikChoi/profile/marlon-alves-UGNVMLyQKtA-unsplash.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="info-content">malon-alves</div>
+                </div>
+                <div className="top-article__info">
+                  <div className="info-image">
+                    <img
+                      src="./images/wonikChoi/profile/rick-gebhardt-Hi_FonIcfjk-unsplash.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="info-content">rick_gebhard</div>
+                </div>
+              </div>
+            </div>
+            <div className="main-article">
+              <div className="main-article__nav">
+                <div className="nav-artivle__container">
+                  <div className="nav-image">
+                    <img
+                      src="./images/wonikChoi/profile/marlon-alves-UGNVMLyQKtA-unsplash.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="nav-title__container">
+                    <div className="nav-title">malon-alves</div>
+                    <div className="nav-subtitle">commacial</div>
+                  </div>
+                </div>
+                <div className="nav-icon">
+                  <i className="bx bx-dots-horizontal-rounded"></i>
+                </div>
+              </div>
+              <div className="main-article__picture">
+                <img src="./images/wonikChoi/picture/food1.jpeg" alt="" />
+              </div>
+              <div className="main-article__heart">
+                <div className="heart-container">
+                  <div className="heart-icon">
+                    <i className="bx bx-heart"></i>
+                  </div>
+                  <div className="heart-icon">
+                    <i className="bx bx-message-rounded"></i>
+                  </div>
+                  <div className="heart-icon">
+                    <i className="bx bx-paper-plane"></i>
+                  </div>
+                </div>
+                <div className="bookmark-container">
+                  <div className="heart-icon">
+                    <i className="bx bx-bookmark"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="main-article__like">
+                <div className="like-span">
+                  좋아요 <span>1,243</span>개
+                </div>
+              </div>
+              <div className="main-article__description">
+                <div className="description-container">
+                  <span>
+                    <span className="userId">malon-alves </span>Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Voluptatum
+                    dignissimos nemo inventore quam nostrum ipsa, doloremque
+                    atque repellendus
+                  </span>
+                </div>
+                <div className="description-comment">
+                  <div className="comment-left">
+                    <div className="user-name">Elvis_lay</div>
+                    <span className="user-comment">Good!</span>
+                  </div>
+                  <div className="comment-right">
+                    <i
+                      className="bx bx-heart"
+                      id="exemple"
+                      onclick="like()"
+                    ></i>
+                  </div>
+                </div>
+                {commentInfo.user.map(message => {
+                  return (
+                    <Comment
+                      key={message.id}
+                      comment={message.comment}
+                      id={message.id}
+                      dispatch={dispatch}
+                    />
+                  );
+                })}
+              </div>
+              <div className="timeline">
+                <span>1일 전</span>
+              </div>
+              <form
+                className="comment-submit"
+                onSubmit={e => {
+                  e.preventDefault();
+                }}
+              >
+                <div className="submit-icon">
+                  <div className="smile-icon"></div>
+                </div>
+                <input
+                  className="comment-input"
+                  type="text"
+                  value={comment}
+                  onChange={e => setComment(e.target.value)}
+                  placeholder="댓글 달기..."
+                />
+                <button
+                  className="comment-btn"
+                  onClick={e => {
+                    dispatch({ type: 'create-message', payload: { comment } });
+                    setComment('');
+                  }}
+                >
+                  게시
+                </button>
+              </form>
+            </div>
+            <div className="main-article">
+              <div className="main-article__nav">
+                <div className="nav-artivle__container">
+                  <div className="nav-image">
+                    <img
+                      src="./images/wonikChoi/profile/rick-gebhardt-Hi_FonIcfjk-unsplash.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="nav-title__container">
+                    <div className="nav-title">rick_gebhard</div>
+                    <div className="nav-subtitle">commacial</div>
+                  </div>
+                </div>
+                <div className="nav-icon">
+                  <i className="bx bx-dots-horizontal-rounded"></i>
+                </div>
+              </div>
+              <div className="main-article__picture">
+                <img src="./images/wonikChoi/picture/cat.jpeg" alt="" />
+              </div>
+              <div className="main-article__heart">
+                <div className="heart-container">
+                  <div className="heart-icon">
+                    <i className="bx bx-heart"></i>
+                  </div>
+                  <div className="heart-icon">
+                    <i className="bx bx-message-rounded"></i>
+                  </div>
+                  <div className="heart-icon">
+                    <i className="bx bx-paper-plane"></i>
+                  </div>
+                </div>
+                <div className="bookmark-container">
+                  <div className="heart-icon">
+                    <i className="bx bx-bookmark"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="main-article__like">
+                <div className="like-span">
+                  좋아요 <span>1,243</span>개
+                </div>
+              </div>
+              <div className="main-article__description">
+                <div className="description-container">
+                  <span>
+                    <span className="userId">rick_gebhard </span>Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Voluptatum
+                    dignissimos nemo inventore quam nostrum ipsa, doloremque
+                    atque repellendus
+                  </span>
+                </div>
+              </div>
+              <div className="timeline">
+                <span>1일 전</span>
+              </div>
+              <form className="comment-submit">
+                <div className="submit-icon">
+                  <div className="smile-icon"></div>
+                </div>
+                <input
+                  className="comment-input"
+                  type="text"
+                  placeholder="댓글 달기..."
+                />
+                <button className="comment-btn" disabled="disabled">
+                  게시
+                </button>
+              </form>
+            </div>
+            <div className="main-article">
+              <div className="main-article__nav">
+                <div className="nav-artivle__container">
+                  <div className="nav-image">
+                    <img
+                      src="./images/wonikChoi/profile/myprofile.jpeg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="nav-title__container">
+                    <div className="nav-title">Yelihi</div>
+                  </div>
+                </div>
+                <div className="nav-icon">
+                  <i className="bx bx-dots-horizontal-rounded"></i>
+                </div>
+              </div>
+              <div className="main-article__picture">
+                <img src="./images/wonikChoi/picture/my.jpeg" alt="" />
+              </div>
+              <div className="main-article__heart">
+                <div className="heart-container">
+                  <div className="heart-icon">
+                    <i className="bx bx-heart"></i>
+                  </div>
+                  <div className="heart-icon">
+                    <i className="bx bx-message-rounded"></i>
+                  </div>
+                  <div className="heart-icon">
+                    <i className="bx bx-paper-plane"></i>
+                  </div>
+                </div>
+                <div className="bookmark-container">
+                  <div className="heart-icon">
+                    <i className="bx bx-bookmark"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="main-article__like">
+                <div className="like-span">
+                  좋아요 <span>1,243</span>개
+                </div>
+              </div>
+              <div className="main-article__description">
+                <div className="description-container">
+                  <span>
+                    <span className="userId">Yelihi </span>위코드 힘들어요..
+                  </span>
+                </div>
+              </div>
+              <div className="timeline">
+                <span>1일 전</span>
+              </div>
+              <form className="comment-submit">
+                <div className="submit-icon">
+                  <div className="smile-icon"></div>
+                </div>
+                <input
+                  className="comment-input"
+                  type="text"
+                  placeholder="댓글 달기..."
+                />
+                <button className="comment-btn" disabled="disabled">
+                  게시
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="main-right">
+            <div className="profile-container">
+              <div className="profile-left">
+                <div className="profile-image">
+                  <img src="./images/wonikChoi/profile/myprofile.jpeg" alt="" />
+                </div>
+                <div className="profile-name">
+                  <span className="profile-user">Yelihi</span>
+                  <span className="profile-fullname">최원익</span>
+                </div>
+              </div>
+              <div className="profile-right">
+                <span>전환</span>
+              </div>
+            </div>
+            <div className="profile-container__separate">
+              <span>회원님을 위한 추천</span>
+              <span>모두 보기</span>
+            </div>
+            <div className="friends-container">
+              <div className="friends">
+                <div className="friends-left">
+                  <div className="friends-image">
+                    <img src="./images/wonikChoi/profile/friend1.jpg" alt="" />
+                  </div>
+                  <div className="friends-name">
+                    <span className="friends-user">realdiegovaldez</span>
+                    <span className="friends-sub">인기</span>
+                  </div>
+                </div>
+                <div className="friends-right">
+                  <span className="follow">팔로우</span>
+                </div>
+              </div>
+              <div className="friends">
+                <div className="friends-left">
+                  <div className="friends-image">
+                    <img src="./images/wonikChoi/profile/friend2.jpg" alt="" />
+                  </div>
+                  <div className="friends-name">
+                    <span className="friends-user">khloekardashian</span>
+                    <span className="friends-sub">인기</span>
+                  </div>
+                </div>
+                <div className="friends-right">
+                  <span className="follow">팔로우</span>
+                </div>
+              </div>
+              <div className="friends">
+                <div className="friends-left">
+                  <div className="friends-image">
+                    <img src="./images/wonikChoi/profile/friend3.jpg" alt="" />
+                  </div>
+                  <div className="friends-name">
+                    <span className="friends-user">simonettalein</span>
+                    <span className="friends-sub">인기</span>
+                  </div>
+                </div>
+                <div className="friends-right">
+                  <span className="follow">팔로우</span>
+                </div>
+              </div>
+              <div className="friends">
+                <div className="friends-left">
+                  <div className="friends-image">
+                    <img src="./images/wonikChoi/profile/friend4.jpg" alt="" />
+                  </div>
+                  <div className="friends-name">
+                    <span className="friends-user">therock</span>
+                    <span className="friends-sub">인기</span>
+                  </div>
+                </div>
+                <div className="friends-right">
+                  <span className="follow">팔로우</span>
+                </div>
+              </div>
+              <div className="friends">
+                <div className="friends-left">
+                  <div className="friends-image">
+                    <img src="./images/wonikChoi/profile/friend6.jpg" alt="" />
+                  </div>
+                  <div className="friends-name">
+                    <span className="friends-user">kyliejenner</span>
+                    <span className="friends-sub">인기</span>
+                  </div>
+                </div>
+                <div className="friends-right">
+                  <span className="follow">팔로우</span>
+                </div>
+              </div>
+            </div>
+            <div className="description">
+              <span>
+                소개&#183;도움말&#183;홍보
+                센터&#183;API&#183;채용정보&#183;개인정보처리방침&#183;약관&#183;위치&#183;언어
+              </span>
+            </div>
+            <div className="copyright">
+              <span>© 2022 INSTAGRAM FROM META</span>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
 
 export default MainWon;

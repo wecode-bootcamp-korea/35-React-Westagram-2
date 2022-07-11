3360587aef5603051117f916ad8bcaeba46450f2
Yelihi <Yelihi19@gmail.com>
2022-07-11 22:11:09 +0900

    Add: mock data 적용

--- src/components/friendBox/FriendBox.jsx
+++ src/components/friendBox/FriendBox.jsx
@@ -3,12 +3,11 @@ import './friendBox.scss';
 import Friend from '../friend/Friend';
 
 export default function FriendBox({ userData }) {
-  console.log(userData);
   return (
     <div className="friendBox">
       <div className="top-article">
         <div className="top-article__container">
-          {userData.friend.map(data => {
+          {userData.map(data => {
             return (
               <Friend key={data.id} userName={data.name} userImg={data.img} />
             );
--- src/pages/wonikChoi/Main/Main.jsx
+++ src/pages/wonikChoi/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Feed from '../../../components/feed/Feed';
 import ProfileBox from '../../../components/profileBox/ProfileBox';
 import RecommendBox from '../../../components/recommendBox/RecommendBox';
@@ -10,87 +10,108 @@ import '../../../styles/reset.scss';
 
 // 검색창 포커스 됬을 경우
 
-const userData = {
-  friend: [
-    {
-      id: Date.now() + Math.random() * 99,
-      name: 'Elvis_lay',
-      img: './images/wonikChoi/profile/elvis-ray-J-JOGvnfgM8-unsplash.jpg',
-      backImg: './images/wonikChoi/picture/cat.jpeg',
-    },
-    {
-      id: Date.now() + Math.random() * 99,
-      name: 'cover-vesi',
-      img: './images/wonikChoi/profile/people1.jpg',
-      backImg: './images/wonikChoi/picture/food1.jpeg',
-    },
-    {
-      id: Date.now() + Math.random() * 99,
-      name: 'malon-alves',
-      img: './images/wonikChoi/profile/marlon-alves-UGNVMLyQKtA-unsplash.jpg',
-      backImg: './images/wonikChoi/picture/food2.jpg',
-    },
-    {
-      id: Date.now() + Math.random() * 99,
-      name: 'rick_gebhard',
-      img: './images/wonikChoi/profile/rick-gebhardt-Hi_FonIcfjk-unsplash.jpg',
-      backImg: './images/wonikChoi/picture/korea.jpg',
-    },
-    {
-      id: Date.now() + Math.random() * 99,
-      name: 'Yelihi',
-      img: './images/wonikChoi/profile/myprofile.jpeg',
-      backImg: './images/wonikChoi/picture/my.jpeg',
-    },
-    {
-      id: Date.now() + Math.random() * 99,
-      name: 'Mendenz',
-      img: './images/wonikChoi/profile/people2.jpg',
-      backImg: './images/wonikChoi/picture/football.jpg',
-    },
-  ],
-};
+// const userData = {
+//   friend: [
+//     {
+//       id: Date.now() + Math.random() * 99,
+//       name: 'Elvis_lay',
+//       img: './images/wonikChoi/profile/elvis-ray-J-JOGvnfgM8-unsplash.jpg',
+//       backImg: './images/wonikChoi/picture/cat.jpeg',
+//     },
+//     {
+//       id: Date.now() + Math.random() * 99,
+//       name: 'cover-vesi',
+//       img: './images/wonikChoi/profile/people1.jpg',
+//       backImg: './images/wonikChoi/picture/food1.jpeg',
+//     },
+//     {
+//       id: Date.now() + Math.random() * 99,
+//       name: 'malon-alves',
+//       img: './images/wonikChoi/profile/marlon-alves-UGNVMLyQKtA-unsplash.jpg',
+//       backImg: './images/wonikChoi/picture/food2.jpg',
+//     },
+//     {
+//       id: Date.now() + Math.random() * 99,
+//       name: 'rick_gebhard',
+//       img: './images/wonikChoi/profile/rick-gebhardt-Hi_FonIcfjk-unsplash.jpg',
+//       backImg: './images/wonikChoi/picture/korea.jpg',
+//     },
+//     {
+//       id: Date.now() + Math.random() * 99,
+//       name: 'Yelihi',
+//       img: './images/wonikChoi/profile/myprofile.jpeg',
+//       backImg: './images/wonikChoi/picture/my.jpeg',
+//     },
+//     {
+//       id: Date.now() + Math.random() * 99,
+//       name: 'Mendenz',
+//       img: './images/wonikChoi/profile/people2.jpg',
+//       backImg: './images/wonikChoi/picture/football.jpg',
+//     },
+//   ],
+// };
 
 const MainWon = () => {
-  return (
-    <>
-      <div className="main">
-        <Nav />
-        <section className="section-container">
-          <div className="main-container">
-            <FriendBox userData={userData} />
+  const [commentList, setCommentList] = useState([]);
+  const [userData, setUserData] = useState([]);
 
-            {userData.friend.map(friend => {
-              return (
-                <Feed
-                  key={friend.id}
-                  userName={friend.name}
-                  userImg={friend.img}
-                  userBackImg={friend.backImg}
-                />
-              );
-            })}
+  useEffect(() => {
+    fetch('http://localhost:3000/data/commentData.json', {
+      method: 'GET', // GET method는 기본값이라서 생략이 가능합니다.
+    })
+      .then(res => res.json())
+      .then(data => {
+        setCommentList(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/data/userData.json', {
+      method: 'GET', // GET method는 기본값이라서 생략이 가능합니다.
+    })
+      .then(res => res.json())
+      .then(data => {
+        setUserData(data);
+      });
+  }, []);
+
+  return (
+    <div className="main">
+      <Nav />
+      <section className="section-container">
+        <div className="main-container">
+          <FriendBox userData={userData} />
+
+          {commentList.map(friend => {
+            return (
+              <Feed
+                key={friend.id}
+                userName={friend.name}
+                userImg={friend.img}
+                userBackImg={friend.backImg}
+              />
+            );
+          })}
+        </div>
+        <div className="main-right">
+          <ProfileBox />
+          <div className="profile-container__separate">
+            <span>회원님을 위한 추천</span>
+            <span>모두 보기</span>
           </div>
-          <div className="main-right">
-            <ProfileBox />
-            <div className="profile-container__separate">
-              <span>회원님을 위한 추천</span>
-              <span>모두 보기</span>
-            </div>
-            <RecommendBox />
-            <div className="description">
-              <span>
-                소개&#183;도움말&#183;홍보
-                센터&#183;API&#183;채용정보&#183;개인정보처리방침&#183;약관&#183;위치&#183;언어
-              </span>
-            </div>
-            <div className="copyright">
-              <span>© 2022 INSTAGRAM FROM META</span>
-            </div>
+          <RecommendBox />
+          <div className="description">
+            <span>
+              소개&#183;도움말&#183;홍보
+              센터&#183;API&#183;채용정보&#183;개인정보처리방침&#183;약관&#183;위치&#183;언어
+            </span>
           </div>
-        </section>
-      </div>
-    </>
+          <div className="copyright">
+            <span>© 2022 INSTAGRAM FROM META</span>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 

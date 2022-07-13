96ddba059a20b9cb1b77b328ba286aa44a2cfe5e
och.changhoon <och.changhoon@gmail.com>
2022-07-14 00:12:06 +0900

    Add : Mission 1, Mission 2 - 사용자 입력 데이터 저장 기능 구현, 로그인 버튼 활성화 기능 구현

--- src/pages/changhoonOh/Login/Login.js
+++ src/pages/changhoonOh/Login/Login.js
@@ -1,30 +1,76 @@
 import Footer from '../../../components/Footer/Footer';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import '../../../styles/reset.scss';
 import './Login.scss';
 
 function Login() {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const [canIPass, canYouPass] = useState(true);
+
+  const handleIdInput = event => {
+    setId(event.target.value);
+  };
+  const handlePwInput = event => {
+    setPw(event.target.value);
+  };
+
+  // const [values, setValues] = useState({
+  //   userId: '',
+  //   userPw: '',
+  //   canIpass: true,
+  // });
+
+  // const handleInput = e => {
+  //   const { name, value } = e.target;
+  //   setValues(prevValues => ({
+  //     ...prevValues,
+  //     [name]: value,
+  //   }));
+  // };
+
+  const validation = () => {
+    return id.includes('@') && pw.length >= 5
+      ? canYouPass(false)
+      : canYouPass(true);
+  };
+
   const navigate = useNavigate();
 
-  const goToMain = () => {
+  const goToMain = e => {
+    e.preventDefault();
     navigate('../Main/Main');
   };
 
   return (
-    <main className="loginMaster">
+    <main className="login">
       <section>
         <div className="leftPanel" />
         <div className="rightPanel">
           <div className="rightTop">
             <h1 className="logo">Instagram</h1>
-            <form className="signIn">
+            <form className="signIn" onKeyUp={validation} onSubmit={goToMain}>
               <input
                 className="id"
+                name="userId"
+                onChange={handleIdInput}
+                onkeyup={validation}
                 type="text"
                 placeholder="전화번호, 사용자 이름 또는 이메일"
               />
-              <input className="pw" type="password" placeholder="비밀번호" />
-              <button className="loginBtn" type="submit" onClick={goToMain}>
+              <input
+                className="pw"
+                name="userPw"
+                onChange={handlePwInput}
+                type="password"
+                placeholder="비밀번호"
+              />
+              <button
+                className={'loginBtn' + (canIPass ? 'Off' : 'On')}
+                type="submit"
+                disabled={canIPass}
+              >
                 로그인
               </button>
               <span className="horizontalLine">
